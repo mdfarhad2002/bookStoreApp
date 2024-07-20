c8dd516ea88d61a96c848efdfdbde6fc4198fbff
Md Farhad <mdfarhad2083@gmail.com>
2024-07-20 12:38:05 +0530

    Files Added

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import React from 'react'
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Cards from "./Cards"
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+        <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+          Welcome to Book Haven, where the love of literature meets the joy of learning. Our bookstore not only offers a diverse selection of books across all genres but also serves as a hub for educational and enrichment courses designed to inspire and educate our community. Here’s a comprehensive overview of the courses we offer.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Course;
